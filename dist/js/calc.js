0fbc038fd3f6d1c57a2d69ea1e4ac54929fa1dc6
jhovannycanas <andres.ceballos@and.gov.co>
2023-01-18 21:11:02 -0500

    base proyect

---
+++ dist/js/calc.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Calc = void 0;
+class Calc {
+    add(a, b) {
+        return a + b;
+    }
+}
+exports.Calc = Calc;
+//# sourceMappingURL=calc.js.map
