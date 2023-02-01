3f9ca5c2aa75e682ca0a4cd792bf3f588dc2ab1e
jhovannycanas <andres.ceballos@and.gov.co>
2023-01-31 20:08:16 -0500

    01 ajustes del listar, editar y eliminar note

--- src/App.ts
+++ src/App.ts
@@ -7,8 +7,8 @@ let setList: HTMLElement = document.getElementById('sect-list') as HTMLElement;
 let txtIdea: HTMLInputElement = document.getElementById('txtIdea') as HTMLInputElement;
 let btnIdea: HTMLButtonElement = document.getElementById('btnIdea') as HTMLButtonElement;
 let btnNew: HTMLButtonElement = document.getElementById('btnNew') as HTMLButtonElement;
+let btnList: HTMLButtonElement = document.getElementById('btnList') as HTMLButtonElement;
 let lsIdea: HTMLTableElement = document.getElementById('ListIdeas') as HTMLTableElement;
-let deleteBans = document.getElementsByClassName("btn-delete");
 let txtTopic: HTMLInputElement = document.getElementById('topic') as HTMLInputElement;
 let txtNote: HTMLTextAreaElement = document.getElementById('notes') as HTMLTextAreaElement;
 let txtSummary: HTMLTextAreaElement = document.getElementById('summary') as HTMLTextAreaElement;
@@ -16,6 +16,7 @@ let btnSave: HTMLButtonElement = document.getElementById('btnSave') as HTMLButto
 let lsNote: HTMLTableElement = document.getElementById('ListNotes') as HTMLTableElement;
 let ideas: Idea[] = [];
 let notes: Note[] = [];
+let idNote: string | null = null;
 
 const add = (ideaText: string) => {
     ideas.push({ id: maxId() + 1, description: ideaText });
@@ -90,10 +91,52 @@ const refreshListNote = () => {
             newItem.innerHTML = `
             <td>${note.topic} </td>
             <td>${note.ideas.map(i => i.description)} </td>
-            <td><input type="button" class="button button-small btn-delete" data-id="${note.id}" value="Edit"/>&nbsp;&nbsp;<input type="button" class="button button-small" data-id="${note.id}" value="X"/></td> `;
+            <td><input type="button" class="button button-small btn-edit" data-id="${note.id}" value="Edit"/>
+            &nbsp;&nbsp;<input type="button" class="button button-small btn-delete btn-delete-note" data-id="${note.id}" value="X"/></td> `;
             lsNote.appendChild(newItem);
-            // ideaDelete(list); Pendiente
+            noteDelete();
+            noteEdit();
         });
+    }
+};
+
+const noteDelete = () => {
+    const removeIdeas = document.querySelectorAll<HTMLInputElement>(".btn-delete-note");
+    removeIdeas.forEach(noteDelete => noteDelete.addEventListener("click", (e) => {
+        removeNote(noteDelete.getAttribute("data-id"));
+        refreshListNote();
+    }));
+}
+const removeNote = (id: string | null) => {
+    if (id) {
+        let index = allNotes().findIndex(i => i.id === id);
+        if (index !== -1) {
+            let notesAll = allNotes();
+            notesAll.splice(index, 1);
+            saveNote(notesAll);
+        }
+    }
+};
+
+const noteEdit = () => {
+    const editIdeas = document.querySelectorAll<HTMLInputElement>(".btn-edit");
+    editIdeas.forEach(noteEdit => noteEdit.addEventListener("click", (e) => {
+        updateNote(noteEdit.getAttribute("data-id"));
+    }));
+}
+
+const updateNote = (id: string | null) => {
+    if (id) {
+        let getNote = allNotes().find(i => i.id === id);
+        if (getNote) {
+            idNote = getNote.id;
+            txtTopic.value = getNote.topic;
+            ideas = getNote.ideas;
+            txtNote.value = getNote.note;
+            txtSummary.value = getNote.summary;
+            visible(setForm);
+            invisible(setList);
+        }
     }
 };
 
@@ -106,16 +149,20 @@ const init = () => {
             add(txtIdea ? txtIdea.value : '');
             refreshList(lsIdea);
             txtIdea ? txtIdea.value = "" : null;
-            deleteBans = document.getElementsByClassName("btn-delete");
         }
     }
     if (btnSave) {
         btnSave.onclick = (e) => {
-            let note = new Note(uuid4(), txtTopic.value, ideas, txtNote.value, txtSummary.value);
+            let note = new Note(idNote ? idNote : uuid4(), txtTopic.value, ideas, txtNote.value, txtSummary.value);
             if (notes.length <= 0) {
                 notes = allNotes();
             }
-            notes.push(note);
+            if (idNote) {
+                let index = allNotes().findIndex(i => i.id === idNote);
+                notes[index] = note;
+            } else {
+                notes.push(note);
+            }
             saveNote(notes);
             invisible(setForm);
             visible(setList);
@@ -127,6 +174,13 @@ const init = () => {
         btnNew.onclick = (e) => {
             visible(setForm);
             invisible(setList);
+        }
+    }
+    if (btnList) {
+        btnList.onclick = (e) => {
+            refreshListNote();
+            invisible(setForm);
+            visible(setList);
         }
     }
 };
