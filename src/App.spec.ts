4e616062650d55e278963c338f63f72c8ddcf10f
jhovannycanas <andres.ceballos@and.gov.co>
2023-02-01 20:07:15 -0500

    01 ajustes y pruebas

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -1,10 +1,96 @@
-import { App } from './App'
+import App from './App';
+import { Note } from './Note';
+
+const mockedElement = {
+    innerHTML: '',
+    addEventListener: jest.fn(),
+    setAttribute: jest.fn(),
+};
 
 describe('App', () => {
-    test('should return 10 for add(6, 4)', () => {
-        expect(new App().add(6, 4)).toBe(10)
-    })
-    test('should return 9 for add(10, -1)', () => {
-        expect(new App().add(10, -1)).toBe(9)
-    })
-})
+    let app: App;
+    let list: HTMLTableElement;
+    let removeIdeas: NodeListOf<HTMLInputElement>;
+
+    beforeEach(() => {
+        const mockGetElementById = jest.fn();
+        jest.spyOn(document, 'getElementById').mockImplementation(mockGetElementById);
+        mockGetElementById.mockReturnValueOnce(mockedElement);
+        app = new App();
+        app.ideas = [{ id: 1, description: 'idea 1' }, { id: 2, description: 'idea 2' }, { id: 3, description: 'idea 3' }];
+        list = document.createElement('table');
+        removeIdeas = document.querySelectorAll<HTMLInputElement>('.button-small');
+    });
+
+    test('add success', () => {
+        const ideaText = 'new idea';
+        app.add(ideaText);
+
+        expect(app.ideas).toHaveLength(4);
+        expect(app.ideas[3]).toEqual({ id: 4, description: ideaText });
+    });
+
+    test('removes an idea with a given id', () => {
+        app.remove(2);
+        expect(app.ideas).toEqual([{ id: 1, description: 'idea 1' }, { id: 3, description: 'idea 3' }]);
+    });
+
+    test('does not remove anything if the id is not found', () => {
+        app.remove(4);
+        expect(app.ideas).toEqual([{ id: 1, description: 'idea 1' }, { id: 2, description: 'idea 2' }, { id: 3, description: 'idea 3' }]);
+    });
+
+    test('should return 0 if the ideas array is empty', () => {
+        app.ideas = [];
+        expect(app.maxId()).toBe(0);
+    });
+
+    test('should return the highest id if the ideas array is not empty', () => {
+        expect(app.maxId()).toBe(3);
+    });
+
+    test('Should refresh the list correctly', () => {
+        const list = document.createElement('table');
+        app.refreshList(list);
+
+        const rows = list.getElementsByTagName('tr');
+        expect(rows.length).toBe(3);
+
+        const firstRow = rows[0];
+        const secondRow = rows[1];
+        const threeRow = rows[2];
+
+        expect(firstRow.id).toBe('1');
+        expect(firstRow.innerHTML).toContain('idea 1');
+
+        expect(secondRow.id).toBe('2');
+        expect(secondRow.innerHTML).toContain('idea 2');
+
+        expect(threeRow.id).toBe('3');
+        expect(threeRow.innerHTML).toContain('idea 3');
+    });
+
+    test('should remove an idea', () => {
+        app.add('new idea');
+        app.ideaDelete(list);
+
+        removeIdeas.forEach(ideaDelete => {
+            ideaDelete.click();
+        });
+
+        expect(app.ideas.length).toBe(4);
+    });
+
+    test('should save notes to local storage', () => {
+        const notes: Note[] = [
+            { id: '1', topic: 'Note 1', ideas: app.ideas, note: 'prueba', summary: 'summarry' },
+            { id: '2', topic: 'Note 2', ideas: app.ideas, note: 'prueba', summary: 'summarry' },
+        ];
+        app.saveNote(notes);
+
+        expect(window.localStorage.getItem('Notes')).toBe(JSON.stringify(notes));
+    });
+
+
+
+});
--- src/App.ts
+++ src/App.ts
@@ -14,173 +14,177 @@ let txtNote: HTMLTextAreaElement = document.getElementById('notes') as HTMLTextA
 let txtSummary: HTMLTextAreaElement = document.getElementById('summary') as HTMLTextAreaElement;
 let btnSave: HTMLButtonElement = document.getElementById('btnSave') as HTMLButtonElement;
 let lsNote: HTMLTableElement = document.getElementById('ListNotes') as HTMLTableElement;
-let ideas: Idea[] = [];
 let notes: Note[] = [];
 let idNote: string | null = null;
 
-const add = (ideaText: string) => {
-    ideas.push({ id: maxId() + 1, description: ideaText });
-};
-
-const remove = (id: number) => {
-    let index = ideas.findIndex(i => i.id === id);
-    if (index !== -1) {
-        ideas.splice(index, 1);
+export default class App {
+    public ideas: Idea[] = [];
+    add(ideaText: string) {
+        this.ideas.push({ id: this.maxId() + 1, description: ideaText });
     }
-};
 
-const maxId = (): number => {
-    return ideas.length <= 0 ? 0 : Math.max(...ideas.map(idea => idea.id));
-};
-
-const refreshList = (list: HTMLTableElement | null) => {
-    if (list) {
-        for (let i = list.rows.length - 1; i > 0; i--) {
-            list.deleteRow(i);
+    remove(id: number) {
+        let index = this.ideas.findIndex(i => i.id === id);
+        if (index !== -1) {
+            this.ideas.splice(index, 1);
         }
-        ideas.forEach((idea): void => {
-            let newItem = document.createElement("tr") as HTMLTableRowElement;
-            newItem.id = idea.id.toString();
-            newItem.innerHTML = `
+    }
+
+    maxId(): number {
+        return this.ideas.length <= 0 ? 0 : Math.max(...this.ideas.map(idea => idea.id));
+    }
+
+    refreshList(list: HTMLTableElement | null) {
+        if (list) {
+            for (let i = list.rows.length - 1; i > 0; i--) {
+                list.deleteRow(i);
+            }
+            this.ideas.forEach((idea): void => {
+                let newItem = document.createElement("tr") as HTMLTableRowElement;
+                newItem.id = idea.id.toString();
+                newItem.innerHTML = `
             <td id="idea${idea.id}" >${idea.description} </td>
             <td><input type="button" class="button button-small btn-delete" data-id="${idea.id}" value="X"/></td> `;
-            list.appendChild(newItem);
-            ideaDelete(list);
-        });
-    }
-};
-
-const ideaDelete = (list: HTMLTableElement | null) => {
-    const removeIdeas = document.querySelectorAll<HTMLInputElement>(".button-small");
-    removeIdeas.forEach(ideaDelete => ideaDelete.addEventListener("click", (e) => {
-        remove((Number(ideaDelete.getAttribute("data-id"))));
-        refreshList(list);
-    }));
-}
-
-//Función para guardar en localstores
-const saveNote = (notes: Note[]) => {
-    if (typeof (Storage) !== "undefined") {
-        window.localStorage.setItem('Notes', JSON.stringify(notes));
-    }
-}
-
-const allNotes = (): Note[] => {
-    const noteStore = window.localStorage.getItem('Notes');
-    if (noteStore !== null) {
-        return JSON.parse(noteStore) as Note[];
-    }
-    return notes;
-}
-
-const visible = (set: HTMLElement) => {
-    set.style.display = 'block';
-}
-const invisible = (set: HTMLElement) => {
-    set.style.display = 'none';
-}
-
-const refreshListNote = () => {
-    if (lsNote) {
-        for (let i = lsNote.rows.length - 1; i > 0; i--) {
-            lsNote.deleteRow(i);
+                list.appendChild(newItem);
+                this.ideaDelete(list);
+            });
         }
-        allNotes().forEach((note): void => {
-            let newItem = document.createElement("tr") as HTMLTableRowElement;
-            newItem.id = note.id.toString();
-            newItem.innerHTML = `
+    }
+
+    ideaDelete(list: HTMLTableElement | null) {
+        const removeIdeas = document.querySelectorAll<HTMLInputElement>(".button-small");
+        removeIdeas.forEach(ideaDelete => ideaDelete.addEventListener("click", (e) => {
+            this.remove((Number(ideaDelete.getAttribute("data-id"))));
+            this.refreshList(list);
+        }));
+    }
+
+    //Función para guardar en localstores
+    saveNote(notes: Note[]) {
+        if (typeof (Storage) !== "undefined") {
+            window.localStorage.setItem('Notes', JSON.stringify(notes));
+        }
+    }
+
+    allNotes(): Note[] {
+        const noteStore = window.localStorage.getItem('Notes');
+        if (noteStore !== null) {
+            return JSON.parse(noteStore) as Note[];
+        }
+        return notes;
+    }
+
+    visible(set: HTMLElement) {
+        set.style.display = 'block';
+    }
+    invisible = (set: HTMLElement) => {
+        set.style.display = 'none';
+    }
+
+    refreshListNote() {
+        if (lsNote) {
+            for (let i = lsNote.rows.length - 1; i > 0; i--) {
+                lsNote.deleteRow(i);
+            }
+            this.allNotes().forEach((note): void => {
+                let newItem = document.createElement("tr") as HTMLTableRowElement;
+                newItem.id = note.id.toString();
+                newItem.innerHTML = `
             <td>${note.topic} </td>
             <td>${note.ideas.map(i => i.description)} </td>
             <td><input type="button" class="button button-small btn-edit" data-id="${note.id}" value="Edit"/>
             &nbsp;&nbsp;<input type="button" class="button button-small btn-delete btn-delete-note" data-id="${note.id}" value="X"/></td> `;
-            lsNote.appendChild(newItem);
-            noteDelete();
-            noteEdit();
-        });
-    }
-};
-
-const noteDelete = () => {
-    const removeIdeas = document.querySelectorAll<HTMLInputElement>(".btn-delete-note");
-    removeIdeas.forEach(noteDelete => noteDelete.addEventListener("click", (e) => {
-        removeNote(noteDelete.getAttribute("data-id"));
-        refreshListNote();
-    }));
-}
-const removeNote = (id: string | null) => {
-    if (id) {
-        let index = allNotes().findIndex(i => i.id === id);
-        if (index !== -1) {
-            let notesAll = allNotes();
-            notesAll.splice(index, 1);
-            saveNote(notesAll);
+                lsNote.appendChild(newItem);
+                this.noteDelete();
+                this.noteEdit();
+            });
         }
-    }
-};
+    };
 
-const noteEdit = () => {
-    const editIdeas = document.querySelectorAll<HTMLInputElement>(".btn-edit");
-    editIdeas.forEach(noteEdit => noteEdit.addEventListener("click", (e) => {
-        updateNote(noteEdit.getAttribute("data-id"));
-    }));
+    noteDelete() {
+        const removeIdeas = document.querySelectorAll<HTMLInputElement>(".btn-delete-note");
+        removeIdeas.forEach(noteDelete => noteDelete.addEventListener("click", (e) => {
+            this.removeNote(noteDelete.getAttribute("data-id"));
+            this.refreshListNote();
+        }));
+    }
+    removeNote(id: string | null) {
+        if (id) {
+            let index = this.allNotes().findIndex(i => i.id === id);
+            if (index !== -1) {
+                let notesAll = this.allNotes();
+                notesAll.splice(index, 1);
+                this.saveNote(notesAll);
+            }
+        }
+    };
+
+    noteEdit() {
+        const editIdeas = document.querySelectorAll<HTMLInputElement>(".btn-edit");
+        editIdeas.forEach(noteEdit => noteEdit.addEventListener("click", (e) => {
+            this.updateNote(noteEdit.getAttribute("data-id"));
+        }));
+    }
+
+    updateNote(id: string | null) {
+        if (id) {
+            let getNote = this.allNotes().find(i => i.id === id);
+            if (getNote) {
+                idNote = getNote.id;
+                txtTopic.value = getNote.topic;
+                this.ideas = getNote.ideas;
+                txtNote.value = getNote.note;
+                txtSummary.value = getNote.summary;
+                this.refreshList(lsIdea);
+                this.visible(setForm);
+                this.invisible(setList);
+            }
+        }
+    };
 }
 
-const updateNote = (id: string | null) => {
-    if (id) {
-        let getNote = allNotes().find(i => i.id === id);
-        if (getNote) {
-            idNote = getNote.id;
-            txtTopic.value = getNote.topic;
-            ideas = getNote.ideas;
-            txtNote.value = getNote.note;
-            txtSummary.value = getNote.summary;
-            refreshList(lsIdea);
-            visible(setForm);
-            invisible(setList);
-        }
-    }
-};
 
 
-const init = () => {
-    visible(setForm);
-    invisible(setList);
+const init = (e: Event) => {
+    let app = new App();
+    app.visible(setForm);
+    app.invisible(setList);
     if (btnIdea) {
         btnIdea.onclick = (e) => {
-            add(txtIdea ? txtIdea.value : '');
-            refreshList(lsIdea);
+            app.add(txtIdea ? txtIdea.value : '');
+            app.refreshList(lsIdea);
             txtIdea ? txtIdea.value = "" : null;
         }
     }
     if (btnSave) {
         btnSave.onclick = (e) => {
-            let note = new Note(idNote ? idNote : uuid4(), txtTopic.value, ideas, txtNote.value, txtSummary.value);
+            let note = new Note(idNote ? idNote : uuid4(), txtTopic.value, app.ideas, txtNote.value, txtSummary.value);
             if (notes.length <= 0) {
-                notes = allNotes();
+                notes = app.allNotes();
             }
             if (idNote) {
-                let index = allNotes().findIndex(i => i.id === idNote);
+                let index = app.allNotes().findIndex(i => i.id === idNote);
                 notes[index] = note;
             } else {
                 notes.push(note);
             }
-            saveNote(notes);
-            invisible(setForm);
-            visible(setList);
-            refreshListNote();
+            app.saveNote(notes);
+            app.invisible(setForm);
+            app.visible(setList);
+            app.refreshListNote();
         }
     }
     if (btnNew) {
         btnNew.onclick = (e) => {
-            visible(setForm);
-            invisible(setList);
+            app.visible(setForm);
+            app.invisible(setList);
         }
     }
     if (btnList) {
         btnList.onclick = (e) => {
-            refreshListNote();
-            invisible(setForm);
-            visible(setList);
+            app.refreshListNote();
+            app.invisible(setForm);
+            app.visible(setList);
         }
     }
 };
